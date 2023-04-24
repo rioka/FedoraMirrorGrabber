b3e3f7b51ab83732793207773fc1886bbd68d666
rioka <rioka@ASUS-GL552>
2023-04-24 20:44:21 +0100

    Add test for `ResponseProcessor`

---
+++ FedoraMirrorGrabber.Core.Tests/Internals/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace FedoraMirrorGrabber.Core.Tests.Internals;
+
+public class Helpers
+{
+  private static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
+  
+  public static string GetResource(string name)
+  {
+    using (var stream = Assembly.GetManifestResourceStream(name))
+    {
+      using (var reader = new StreamReader(stream))
+      {
+        return reader.ReadToEnd();
+      }
+    }
+  }
+}
