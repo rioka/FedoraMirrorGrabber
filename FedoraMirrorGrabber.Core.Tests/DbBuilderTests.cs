3c2e0670eea0a3e7ade693f68a0453d4368600ee
rioka <rioka@ASUS-GL552>
2023-04-24 22:08:16 +0100

    More tests for DbBuilder

--- FedoraMirrorGrabber.Core.Tests/DbBuilderTests.cs
+++ FedoraMirrorGrabber.Core.Tests/DbBuilderTests.cs
@@ -33,5 +33,26 @@ public class DbBuilderTests
     Assert.That(lines.Count(), Is.EqualTo(2));
     Assert.That(lines[0], Is.EqualTo(@"^http:\/\/creeperhost\.mm\.fcix\.net\/fedora\/linux\/releases\/37\/Everything\/(x86_64\/[a-zA-Z0-9\-\_\.\/]+\.d?rpm)$	http://repo.mirrors.squid.internal/fedora/37/$1"));
     Assert.That(lines[1], Is.EqualTo(@"^https:\/\/mirror\.vpsnet\.com\/fedora\/linux\/releases\/37\/Everything\/(x86_64\/[a-zA-Z0-9\-\_\.\/]+\.d?rpm)$	http://repo.mirrors.squid.internal/fedora/37/$1"));
+  }
+  
+  
+  [Test]
+  public async Task Save_creates_a_file_applying_selector()
+  {
+    // arrange
+    var mirrors = new List<Mirror>() {
+      new Mirror("http://creeperhost.mm.fcix.net/fedora/linux/releases/37/Everything/x86_64/os/repodata/repomd.xml", RepositoryType.Http), 
+      new Mirror("https://mirror.vpsnet.com/fedora/linux/releases/37/Everything/x86_64/os/repodata/repomd.xml", RepositoryType.Https),
+      new Mirror("http://distrib-coffee.ipsl.jussieu.fr/pub/linux/fedora/linux/releases/37/Everything/x86_64/os/repodata/repomd.xml", RepositoryType.Http)
+    };
+    
+    // act
+    await _sut.Save("fedora.db", "x86_64", 37, mirrors, m => m.Type == RepositoryType.Http); 
+    
+    // assert
+    var lines = await _fileSystem.File.ReadAllLinesAsync("fedora.db");
+    Assert.That(lines.Count(), Is.EqualTo(2));
+    Assert.That(lines[0], Is.EqualTo(@"^http:\/\/creeperhost\.mm\.fcix\.net\/fedora\/linux\/releases\/37\/Everything\/(x86_64\/[a-zA-Z0-9\-\_\.\/]+\.d?rpm)$	http://repo.mirrors.squid.internal/fedora/37/$1"));
+    Assert.That(lines[1], Is.EqualTo(@"^http:\/\/distrib-coffee\.ipsl\.jussieu\.fr\/pub\/linux\/fedora\/linux\/releases\/37\/Everything\/(x86_64\/[a-zA-Z0-9\-\_\.\/]+\.d?rpm)$	http://repo.mirrors.squid.internal/fedora/37/$1"));
   }  
 }
