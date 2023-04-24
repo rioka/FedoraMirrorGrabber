c84c90333e761f30209cb61e6fccceaab73519a0
rioka <rioka@libero.it>
2023-04-24 15:44:32 +0100

    Add core components

---
+++ FedoraMirrorGrabber.Core/MirrorListRetriever.cs
@@ -0,0 +1,37 @@
+﻿namespace FedoraMirrorGrabber.Core;
+
+public class MirrorListRetriever
+{
+  #region Data
+
+  public static readonly string Host = "https://mirrors.fedoraproject.org/";
+  private readonly HttpClient _client;
+  private readonly ResponseProcessor _processor;
+
+  #endregion
+
+  public Uri BaseAddress { get; set; }
+
+  #region Constructors
+  
+  public MirrorListRetriever(HttpClient client, ResponseProcessor processor)
+  {
+    _client = client;
+    _processor = processor;
+    BaseAddress = new Uri(Host);
+  }
+
+  #endregion
+
+  #region APIs
+
+  public async Task<IEnumerable<Mirror>> GetMirrors(string baseArch, int releaseVersion)
+  {
+    _client.BaseAddress = new Uri("https://mirrors.fedoraproject.org/");
+    var content = await _client.GetStringAsync($"metalink?repo=fedora-{releaseVersion}&arch={baseArch}");
+
+    return _processor.Run(content);
+  }
+
+  #endregion
+}
