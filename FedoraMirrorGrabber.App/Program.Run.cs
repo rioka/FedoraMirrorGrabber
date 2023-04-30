e11e2b7c72cfe854f7ad97252223336902da1f25
rioka <rioka@ASUS-GL552>
2023-04-30 17:06:52 +0100

    Initial code for application

---
+++ FedoraMirrorGrabber.App/Program.Run.cs
@@ -0,0 +1,25 @@
+﻿using FedoraMirrorGrabber.App;
+using FedoraMirrorGrabber.Core;
+using System.IO.Abstractions;
+
+internal partial class Program
+{
+  public static async Task Run(Options options)
+  {
+      var client = new HttpClient() ;
+      var retriever = new MirrorListRetriever(client, new ResponseProcessor()) {
+        BaseAddress = new Uri(options.Url)
+      };
+      var mirrors = await retriever.GetMirrors(options.Architecture, options.ReleaseVersion);
+
+      Func<Mirror, bool>? filter = default;
+      if (options.Protocols is not null && options.Protocols.Any())
+      {
+        filter = m => options.Protocols.Contains(m.Type);
+      }
+
+      var fs = new FileSystem();
+      var builder = new DbBuilder(fs);
+      await builder.Save(options.SaveTo, options.Architecture, options.ReleaseVersion, mirrors, filter);  
+  } 
+}
---
+++ FedoraMirrorGrabber.App/Program.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+using FedoraMirrorGrabber.App;
+
+internal partial class Program
+{
+  public static async Task Main(string[] args)
+  {
+    await Parser
+      .Default
+      .ParseArguments<Options>(args)
+      .WithParsedAsync(Run);
+  }
+}
