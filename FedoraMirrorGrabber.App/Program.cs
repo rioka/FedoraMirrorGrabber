71c2c60df2c265d3e7e22b9b161344cbd91784d0
rioka <rioka@ASUS-GL552>
2023-05-22 21:13:25 +0100

    WIP

--- FedoraMirrorGrabber.App/Program.cs
+++ FedoraMirrorGrabber.App/Program.cs
@@ -1,5 +1,9 @@
 ﻿using CommandLine;
 using FedoraMirrorGrabber.App;
+using FedoraMirrorGrabber.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO.Abstractions;
 
 internal partial class Program
 {
@@ -10,4 +14,25 @@ internal partial class Program
       .ParseArguments<Options>(args)
       .WithParsedAsync(Run);
   }
+  
+  private static IHostBuilder CreateHostBuilder(string[] args)
+  {
+    var builder = Host
+      .CreateDefaultBuilder(args)              
+      .ConfigureServices((services) => {
+
+        services.AddHttpClient<MirrorListRetriever>();
+
+        services
+          .AddScoped<IFileSystem, FileSystem>()
+          .AddScoped<DbBuilder>()
+          .AddScoped<MirrorListRetriever>()
+          .AddScoped<ResponseProcessor>();
+
+        //services.AddScoped<SampleApp>();
+      });
+  
+    return builder;
+  }        
+  
 }
