c220381d4899a8f3d54286546a82a10e4a7b5c1a
rioka <rioka@libero.it>
2025-03-24 16:34:02 +0000

    Formatting

--- FedoraMirrorGrabber.Core/Mirror.cs
+++ FedoraMirrorGrabber.Core/Mirror.cs
@@ -4,7 +4,7 @@ namespace FedoraMirrorGrabber.Core;
 
 public class Mirror
 {
-  [SuppressMessage("NDepend", "ND2209:UriFieldsShouldBeOfTypeSystemUri", Justification="This is a string representation of a URI.")]
+  [SuppressMessage("NDepend", "ND2209:UriFieldsShouldBeOfTypeSystemUri", Justification = "This is a string representation of a URI.")]
   public string Url { get; }
 
   public RepositoryType Type { get; }
