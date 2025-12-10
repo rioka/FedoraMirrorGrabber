95bd08c9cb6efa952fa18b044502cee89d229640
rioka <rioka@libero.it>
2025-12-10 17:24:17 +0000

    Initial implementation

--- FedoraMirrorGrabber.Core/Builders/AptCacherDbBuilder.cs
+++ FedoraMirrorGrabber.Core/Builders/AptCacherDbBuilder.cs
@@ -11,8 +11,27 @@ public class AptCacherDbBuilder : IDbBuilder
     _fileSystem = fileSystem;
   }
 
-  public Task Save(string saveTo, string baseArch, int releaseVersion, IEnumerable<Mirror> mirrors, Func<Mirror, bool>? selector = null)
+  #region IDbBuilder
+
+  public async Task Save(string saveTo, string baseArch, int releaseVersion, IEnumerable<Mirror> mirrors, Func<Mirror, bool>? selector = null)
   {
-    throw new NotImplementedException();
+    await using (var stream = _fileSystem.File.CreateText(saveTo))
+    {
+      selector ??= _ => true;
+      // TODO check if this is the correct tag to look for
+      var pattern = $"/{baseArch}";
+
+      foreach (var mirror in mirrors
+                 .Where(m => selector(m)))
+      {
+        // var regex = BuildRegex(url, baseArch);
+        // var entry = BuildStoreIdEntry(regex, releaseVersion);
+        await stream.WriteAsync(mirror.Url.TrimAt(pattern));
+      }
+      
+      await stream.FlushAsync();
+    }
   }
+
+  #endregion
 }
