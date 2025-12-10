f944b99b6a70f6389b014b009c6b41b3a8ba819d
rioka <rioka@libero.it>
2025-12-10 16:39:55 +0000

    Add `AptCacherDbBuilder.cs`

---
+++ FedoraMirrorGrabber.Core/AptCacherDbBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.IO.Abstractions;
+
+namespace FedoraMirrorGrabber.Core;
+
+public class AptCacherDbBuilder : IDbBuilder
+{
+  private readonly IFileSystem _fileSystem;
+
+  public AptCacherDbBuilder(IFileSystem fileSystem)
+  {
+    _fileSystem = fileSystem;
+  }
+
+  public Task Save(string saveTo, string baseArch, int releaseVersion, IEnumerable<Mirror> mirrors, Func<Mirror, bool>? selector = null)
+  {
+    throw new NotImplementedException();
+  }
+}
